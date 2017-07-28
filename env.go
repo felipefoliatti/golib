fb17cb13f0e5f4ba53501680b2db6b83da39bd46
Felipe Foliatti <felipe.foliatti@soha.com.br>
2017-07-27 23:20:47 -0300

    envirnoment functions

--- env.go
+++ env.go
@@ -2,7 +2,9 @@ package golib
 
 import "os"
 
-func getenv(key, fallback string) string {
+//Getenv helps to handle null value.
+//If the env variable doesn't exist, then the fallback string is returned
+func Getenv(key, fallback string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
 		return fallback
