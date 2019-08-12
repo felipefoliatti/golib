c4ee1aa84459f5d240e4792cdd1b8d49d2b49cc3
Felipe Foliatti <felipe.foliatti@gmail.com>
2019-08-12 17:10:03 -0300

    added default get to http

--- http.go
+++ http.go
@@ -5,8 +5,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net"
 	"net/http"
 	"strings"
 	"time"
@@ -26,6 +24,9 @@ func Patch(logger Logger, url string, obj interface{}, target interface{}, heade
 }
 func Head(logger Logger, url string, obj interface{}, target interface{}, headers map[string]string) *errors.Error {
 	return request("HEAD", logger, url, obj, target, headers)
+}
+func Get(logger Logger, url string, target interface{}, headers map[string]string) *errors.Error {
+	return request("GET", logger, url, nil, target, headers)
 }
 
 func request(method string, logger Logger, url string, obj interface{}, target interface{}, headers map[string]string) *errors.Error {
@@ -102,71 +103,6 @@ func request(method string, logger Logger, url string, obj interface{}, target i
 					err = errors.WrapInner("error marshalling", e, 0)
 
 					return err
-				}
-			}
-		}
-
-		return err
-
-	}, backoff.WithMaxRetries(backoff.NewExponentialBackOff(), 3))
-
-	if err == nil {
-		return nil
-	}
-	return err.(*errors.Error)
-}
-
-func Get(logger Logger, url string, target interface{}, headers map[string]string) *errors.Error {
-	err := backoff.Retry(func() error {
-
-		var e error
-		var err *errors.Error
-
-		var resp *http.Response
-		var req *http.Request
-
-		req, e = http.NewRequest("GET", url, nil)
-		err = errors.WrapInner("error creating the request", e, 0)
-
-		if err == nil {
-			req.Header.Set("Content-Type", "application/json")
-
-			if headers != nil {
-				for key, value := range headers {
-					req.Header.Add(key, value)
-				}
-			}
-
-			client := &http.Client{Timeout: time.Second * 10, Transport: &http.Transport{Dial: (&net.Dialer{Timeout: 5 * time.Second}).Dial, TLSHandshakeTimeout: 5 * time.Second}}
-			resp, e = client.Do(req)
-			err = errors.WrapInner("error requesting url", e, 0)
-
-			if err == nil {
-
-				defer resp.Body.Close()
-
-				if resp.StatusCode == 200 || resp.StatusCode == 202 {
-
-					//var response Response
-					e = json.NewDecoder(resp.Body).Decode(&target)
-					err = errors.WrapInner("error decoding json", e, 0)
-
-					//logger.LogAIf2(e != nil, WARN, Data{Message: "error parsing json from request ( " + url + "): " + TryError(e)})
-
-					return err
-
-				} else {
-
-					body, e := ioutil.ReadAll(resp.Body)
-
-					if e == nil {
-						err = errors.Errorf("error requesting url - body:" + string(body))
-					} else {
-						err = errors.WrapInner("error requesting url - not possible to parse the body", e, 0)
-					}
-
-					return err
-
 				}
 			}
 		}
