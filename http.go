721505206fd6087949c90611b546a377e3ec4856
Felipe Foliatti <felipe.foliatti@soha.com.br>
2025-04-08 11:20:37 -0300

    feat: added body print as json to http

--- http.go
+++ http.go
@@ -123,40 +123,40 @@ func request(method string, logger Logger, url string, obj interface{}, target i
 						_ = json.Unmarshal([]byte(b), &target)
 					}
 
-					//try to parse object
-					var ojson *string
-					var obytes []byte
-
-					//try to parse body to json
-					obytes, e = json.Marshal(body)
-
-					if e == nil {
-						temp := string(obytes)
-						ojson = &temp
-					}
-
-					var pbody string = ""
-
-					//select the information to print
-					if ojson == nil {
-						//if it was not possible to convert body to json, print the string
-						//cleans up the string to print
-						pbody = body
-					} else {
-						//otherwise print the json
-						pbody = *ojson
-					}
-
 					//cleans up the string to print
-					pbody = strings.Replace(pbody, "\"", "'", -1)
-
+					pbody := strings.Replace(body, "\"", "'", -1)
 					var baseErr error
+
 					//create a base error (or a caused by)
 					if obj == nil {
-
 						baseErr = fmt.Errorf("error in service - %s %s -> code %d and body %s", method, url, resp.StatusCode, pbody)
 					} else {
-						baseErr = fmt.Errorf("error in service - %s %s - body: %v -> code %d and body %s", method, url, obj, resp.StatusCode, pbody)
+
+						//try to parse object
+						var ojson *string
+						var obytes []byte
+
+						//try to parse body to json
+						obytes, e = json.Marshal(obj)
+
+						if e == nil {
+							temp := string(obytes)
+							ojson = &temp
+						}
+
+						var pobj string = ""
+
+						//select the information to print
+						if ojson == nil {
+							//if it was not possible to convert body to json, print the string
+							//cleans up the string to print
+							pobj = fmt.Sprintf("%v", obj)
+						} else {
+							//otherwise print the json
+							pobj = *ojson
+						}
+
+						baseErr = fmt.Errorf("error in service - %s %s - body: %v -> code %d and body %s", method, url, pobj, resp.StatusCode, pbody)
 					}
 
 					//try to convert to error
